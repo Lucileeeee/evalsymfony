aecde1de060d8e600e3e25359745240c2ab55abb
Your Name <you@example.com>
2025-03-28 16:18:47 +0100

    retouche partie 1

--- src/Entity/Task.php
+++ src/Entity/Task.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\TaskRepository;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Boolean;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
@@ -29,14 +30,23 @@ class Task
     )]
     private ?string $title =  null;
 
-   
-
     #[ORM\Column(type: Types::TEXT)]
     #[Assert\NotBlank(
         message:"Le contenu doit être non vide"
     )]
     private ?string $content = null;
 
+    #[ORM\Column]
+    private ?\DateTimeImmutable $createAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $expiredAt = null;
+
+    #[ORM\Column]
+    private Boolean $status = null;
+
+
+//GETTER ET SETTER
     public function getId(): ?int
     {
         return $this->id;
@@ -62,6 +72,41 @@ class Task
     public function setContent(string $content): static
     {
         $this->content = $content;
+
+        return $this;
+    }
+
+    public function getCreateAt(): ?\DateTimeImmutable
+    {
+        return $this->createAt;
+    }
+
+    public function setCreateAt(\DateTimeImmutable $createAt): static
+    {
+        $this->createAt = $createAt;
+
+        return $this;
+    }
+    public function getExpiredAt(): ?\DateTimeImmutable
+    {
+        return $this->expiredAt;
+    }
+
+    public function setExpiredAt(\DateTimeImmutable $expiredAt): static
+    {
+        $this->expiredAt = $expiredAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?Boolean
+    {
+        return $this->status;
+    }
+
+    public function setStatus(Boolean $status): static
+    {
+        $this->status = $status;
 
         return $this;
     }
